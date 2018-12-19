19c16f25574a761714b4d93838f2ba0b5970f88a
lydia <liyingdi1992@163.com>
2018-12-19 11:31:34 +0800

    返回json类型

--- sysInfo_centos.go
+++ sysInfo_centos.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"strings"
 	"strconv"
+	"encoding/json"
 )
 
 func main() {
@@ -18,13 +19,14 @@ func main() {
 	Info:=sysInfo()
 	fmt.Println(Info)
 
+
 }
 
-func sysInfo()map[string]string{
-    info:=make(map[string]string)
-	dis:=make(map[string]string)
-	//cp:=make(map[string]string)
-	//memory:=make(map[string]string)
+func sysInfo()string{
+	info:=make(map[string]map[string]string)
+	//dis:=make(map[string]string)
+	cp:=make(map[string]string)
+	memory:=make(map[string]string)
 	cmd :=exec.Command("df","-hP")
 	out,err:=cmd.CombinedOutput()
 	if err!=nil{
@@ -40,14 +42,16 @@ func sysInfo()map[string]string{
 			if err!=nil{
 				fmt.Println("err")
 			}
-			dis["Path"]=d.Path
-			dis["diskTotal"]=strconv.Itoa(int(d.Total/1024/1024/1024))
-			dis["diskFree"]=strconv.Itoa(int(d.Free/1024/1024/1024))
-			dis["diskUsed"]=strconv.Itoa(int(d.Used/1024/1024/1024))
-			dis["diskUsedPercent"]=strconv.Itoa(int(d.UsedPercent))
-			for k,v:=range dis{
-				info[k]=v
-			}
+			//dis["Path"]=d.Path
+			//dis["diskTotal"]=strconv.Itoa(int(d.Total/1024/1024/1024))
+			//dis["diskFree"]=strconv.Itoa(int(d.Free/1024/1024/1024))
+			//dis["diskUsed"]=strconv.Itoa(int(d.Used/1024/1024/1024))
+			//dis["diskUsedPercent"]=strconv.Itoa(int(d.UsedPercent))
+			ds := map[string]string{"diskTotal":strconv.Itoa(int(d.Total/1024/1024)),
+				"diskUsed":strconv.Itoa(int(d.Used/1024/1024)),"diskFree":strconv.Itoa(int(d.Free/1024/1024)),
+				"diskUsedPercent":strconv.Itoa(int(d.UsedPercent))}
+
+			info[d.Path]=ds
 			//aa:=make(map[string]string)
 
 			//info["disk"]=dis
@@ -64,17 +68,24 @@ func sysInfo()map[string]string{
 
 	//fmt.Printf("UsedPercent:%.2f",UsedPercent)
 	//fmt.Println("MemoryTotal:",m.Total/1024/1024,"M","Used:",(m.Total-m.Free-m.Buffers-m.Cached)/1024/1024,"M","UsedPercent:",UsedPercent*100,"%","Cached:",m.Cached/1024/1024,"M","SwapTotal:",m.SwapTotal/1024/1024,"M","SwapFree",m.SwapFree/1024/1024,"M")
-	info["memoryTotal"]=strconv.Itoa(int(m.Total/1024/1024))
-	info["memoryUsed"]=strconv.Itoa(int((m.Total-m.Free-m.Buffers-m.Cached)/1024/1024))
-	info["memoryUsedPercent"]=strconv.Itoa(int(UsedPercent*100))
-	info["memoryCached"]=strconv.Itoa(int(m.Cached/1024/1024))
-	info["SwapTotal"]=strconv.Itoa(int(m.SwapTotal/1024/1024))
-	info["SwapFree"]=strconv.Itoa(int(m.SwapFree/1024/1024))
-	//info["memory"]=memory
-    i,err:=cpu.Counts(true)
+	memory["memoryTotal"]=strconv.Itoa(int(m.Total/1024/1024))
+	memory["memoryUsed"]=strconv.Itoa(int((m.Total-m.Free-m.Buffers-m.Cached)/1024/1024))
+	memory["memoryUsedPercent"]=strconv.Itoa(int(UsedPercent*100))
+	memory["memoryCached"]=strconv.Itoa(int(m.Cached/1024/1024))
+	memory["SwapTotal"]=strconv.Itoa(int(m.SwapTotal/1024/1024))
+	memory["SwapFree"]=strconv.Itoa(int(m.SwapFree/1024/1024))
+	info["memory"]=memory
+	i,err:=cpu.Counts(true)
 	//fmt.Println("CPU:",i)
-	info["cpuCounts"]=strconv.Itoa(int(i))
-	//info["cpu"]=cp
-	return info
+	cp["cpuCounts"]=strconv.Itoa(int(i))
+	info["cpu"]=cp
+	s,err:=json.Marshal(info)
+	if err!=nil{
+		fmt.Println("error")
+	}
+	ss:=string(s)
+	return ss
+
+	//return info
 
 }
